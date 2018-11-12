d4be883f57e7c4107fcdd80811400b74602e0446
Petr Kudinov <43792195+petrkudinov@users.noreply.github.com>
2018-11-13 00:24:54 +0500

    Update huerifma.py

--- huerifma.py
+++ huerifma.py
@@ -1,77 +1,186 @@
-cnt = 0
 a = ''
 while a != 'хуй':
-    a = str(input())
+    a = str(input().lower())
     if a == 'хуй':
-        print('лучшая рифма к слову "хуй" - "хуй"')
-        cnt += 1
+        print('ты пидор!')
     elif a[0] == 'а':
         print('ху', end='')
         print('я', end='')
         print(a[1:])
-        cnt += 1
     elif a[0] == 'ы':
         print('ху', end='')
         print('и', end='')
         print(a[1:])
-        cnt += 1
     elif a[0] == 'о':
         print('ху', end='')
         print('ё', end='')
         print(a[1:])
-        cnt += 1
     elif a[0] == 'у':
         print('ху', end='')
         print('ю', end='')
         print(a[1:])
-        cnt += 1
     elif a[0] == 'э':
         print('ху', end='')
         print('е', end='')
-        print(a[1:])
-        cnt += 1        
+        print(a[1:])     
     elif a[0] == 'я':
         print('ху', end='')
         print(a[0:])
-        cnt += 1
+    elif a[0] == 'и':
+        print('ху', end='')
+        print(a[0:])
     elif a[0] == 'ё':
         print('ху', end='')
         print(a[0:])
-        cnt += 1
     elif a[0] == 'ю':
         print('ху', end='')
         print(a[0:])
-        cnt += 1
     elif a[0] == 'е':
         print('ху', end='')
-        print(a[0:])
-        cnt += 1        
+        print(a[0:])       
     elif a[1] == 'а':
         print('ху', end='')
         print('я', end='')
         print(a[2:])
-        cnt += 1
     elif a[1] == 'ы':
         print('ху', end='')
         print('и', end='')
         print(a[2:])
-        cnt += 1
     elif a[1] == 'о':
         print('ху', end='')
         print('ё', end='')
         print(a[2:])
-        cnt += 1
     elif a[1] == 'у':
         print('ху', end='')
         print('ю', end='')
         print(a[2:])
-        cnt += 1
     elif a[1] == 'э':
         print('ху', end='')
         print('е', end='')
         print(a[2:])
-        cnt += 1
+    elif a[1] == 'я':
+        print('ху', end='')
+        print(a[1:])
+    elif a[1] == 'и':
+        print('ху', end='')
+        print(a[1:])
+    elif a[1] == 'ё':
+        print('ху', end='')
+        print(a[1:])
+    elif a[1] == 'ю':
+        print('ху', end='')
+        print(a[1:])
+    elif a[1] == 'е':
+        print('ху', end='')
+        print(a[1:])     
+    elif a[0] and a[1] == 'б' or 'в' or 'г' or 'д' or 'ж' or 'з' or 'й' or 'к' or 'л' or 'м' or 'н' or 'п' or 'р' or 'с' or 'т' or 'ф' or 'х' or 'ц' or 'ч' or 'ш' or 'щ' or 'ъ' or 'ь':
+        if a[2] == 'а':
+            print('ху', end='')
+            print('я', end='')
+            print(a[3:])
+        elif a[2] == 'о':
+            print('ху', end='')
+            print('ё', end='')
+            print(a[3:])
+        elif a[2] == 'у':
+            print('ху', end='')
+            print('ю', end='')
+            print(a[3:])
+        elif a[2] == 'э':
+            print('ху', end='')
+            print('е', end='')
+            print(a[3:])
+        elif a[2] == 'ы':
+            print('ху', end='')
+            print('и', end='')
+            print(a[3:])
+        elif a[2] == 'я':
+            print('ху', end='')
+            print(a[2:])
+        elif a[2] == 'ё':
+            print('ху', end='')
+            print(a[2:])
+        elif a[2] == 'ю':
+            print('ху', end='')
+            print(a[2:])
+        elif a[2] == 'е':
+            print('ху', end='')
+            print(a[2:])
+        elif a[2] == 'и':
+            print('ху', end='')
+            print(a[2:])
+        elif a[0] and a[1] and a[2] == 'б' or 'в' or 'г' or 'д' or 'ж' or 'з' or 'й' or 'к' or 'л' or 'м' or 'н' or 'п' or 'р' or 'с' or 'т' or 'ф' or 'х' or 'ц' or 'ч' or 'ш' or 'щ' or 'ъ' or 'ь':
+            if a[3] == 'а':
+                print('ху', end='')
+                print('я', end='')
+                print(a[4:])
+            elif a[3] == 'о':
+                print('ху', end='')
+                print('ё', end='')
+                print(a[4:])
+            elif a[3] == 'у':
+                print('ху', end='')
+                print('ю', end='')
+                print(a[4:])
+            elif a[3] == 'э':
+                print('ху', end='')
+                print('е', end='')
+                print(a[4:])
+            elif a[3] == 'ы':
+                print('ху', end='')
+                print('и', end='')
+                print(a[4:])
+            elif a[3] == 'я':
+                print('ху', end='')
+                print(a[3:])
+            elif a[3] == 'ё':
+                print('ху', end='')
+                print(a[3:])
+            elif a[3] == 'ю':
+                print('ху', end='')
+                print(a[3:])
+            elif a[3] == 'е':
+                print('ху', end='')
+                print(a[3:])
+            elif a[3] == 'и':
+                print('ху', end='')
+                print(a[3:])
+            elif a[0] and a[1] and a[2] and a[3] == 'б' or 'в' or 'г' or 'д' or 'ж' or 'з' or 'й' or 'к' or 'л' or 'м' or 'н' or 'п' or 'р' or 'с' or 'т' or 'ф' or 'х' or 'ц' or 'ч' or 'ш' or 'щ' or 'ъ' or 'ь':
+                if a[4] == 'а':
+                    print('ху', end='')
+                    print('я', end='')
+                    print(a[5:])
+                elif a[4] == 'о':
+                    print('ху', end='')
+                    print('ё', end='')
+                    print(a[5:])
+                elif a[4] == 'у':
+                    print('ху', end='')
+                    print('ю', end='')
+                    print(a[5:])
+                elif a[4] == 'э':
+                    print('ху', end='')
+                    print('е', end='')
+                    print(a[5:])
+                elif a[4] == 'ы':
+                    print('ху', end='')
+                    print('и', end='')
+                    print(a[5:])
+                elif a[4] == 'я':
+                    print('ху', end='')
+                    print(a[4:])
+                elif a[4] == 'ё':
+                    print('ху', end='')
+                    print(a[4:])
+                elif a[4] == 'ю':
+                    print('ху', end='')
+                    print(a[4:])
+                elif a[4] == 'е':
+                    print('ху', end='')
+                    print(a[4:])
+                elif a[4] == 'и':
+                    print('ху', end='')
+                    print(a[4:])
     else:
         print('ху', end='')
         print(a[1:])
-        cnt += 1
